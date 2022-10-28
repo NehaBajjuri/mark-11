91b525a520ce70d7202f15c6e2eb494cac6c7260
NehaBajjuri <105218064+NehaBajjuri@users.noreply.github.com>
2022-10-28 22:29:56 +0530

    changes

--- app.js
+++ app.js
@@ -1,30 +1,54 @@
 var dateOfBirth = document.querySelector("#date-of-birth");
 var luckyNum = document.querySelector("#lucky-number");
 var checkNumberButton = document.querySelector("#check-number");
+var output = document.querySelector("#out-put");
 
-
-function compareValues(sum,luckyNumber)
-{
-    //console.log(sum,luckyNumber);
+// function compare(sum, luckyNum)
+// {
+//     if(sum % luckyNum == 0)  
+//     {
+//         output.innerText = "Your birthday is luckyyy!!";
+//     }
+//     else{
+//         output.innerText = "Your birthday is not so lucky:/" ;
+//     }
+// }
+function showMessage(message) {
+    output.innerText = message;
 }
+checkNumberButton.addEventListener("click", checkIfLucky);
 function checkIfLucky(){
-    const dob = dateOfBirth.value;
-    const sum = calculateSum(dob);
-    console.log(sum);
+    var dob = dateOfBirth.value;
+    var sum = calculateSum(dob);
+    var num = luckyNum.value;
+    if(dob && num){
+   // compare(sum,luckyNum.value);
+   
+   if(sum % luckyNum == 0)  
+   {
+       output.innerText = "Your birthday is luckyyy!!";
+   }
+   else {
+       output.innerText = "Your birthday is not so lucky:/" ;
+   }
+ }
+            else{
+      output.innerText = "Please enter both the fields";  
+            }
+    
 }
+
+  
+
 
 function calculateSum(dob){
     dob = dob.replaceAll("-","");
+   
     let sum=0;
+
     for(let index=0;index<dob.length;index++)
     {
         sum = sum + Number(dob.charAt(index));
     }
     return sum;
 }
-
-checkNumberButton.addEventListener('click',function
-    getInputs(){
-       // console.log(dateOfBirth.value,luckyNum.value)
-    
-})
